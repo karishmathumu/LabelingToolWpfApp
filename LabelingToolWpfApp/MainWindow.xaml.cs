07be2ed40f7283cfb05ed3bbd4a3e0b3467580d0
karishmathumu <karishmathumu@gmail.com>
2023-07-03 14:10:58 +0200

    Added Mouse Up, Down, Move controls

--- LabelingToolWpfApp/MainWindow.xaml.cs
+++ LabelingToolWpfApp/MainWindow.xaml.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+
 using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
+
 
 
 namespace UIDesignWPFApp
@@ -28,12 +27,18 @@ namespace UIDesignWPFApp
         private List<string> imageFiles;
         private int currentImageIndex;
 
+        private Point startPoint;
+        private Rectangle currentRectangle;
+
+        private List<Rect> boundingBoxes = new List<Rect>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+        
         }
-
-
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
@@ -67,22 +72,32 @@ namespace UIDesignWPFApp
 
         private void PrevButton_Click(object sender, RoutedEventArgs e)
         {
+         
+            SaveBoundingBoxes();
+     
+
             currentImageIndex--;
             if (currentImageIndex < 0)
             {
                 currentImageIndex = imageFiles.Count - 1;
             }
+           
             DisplayCurrentImage();
             UpdateImageCount();
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
+            
+            SaveBoundingBoxes();
+     
+
             currentImageIndex++;
             if (currentImageIndex >= imageFiles.Count)
             {
                 currentImageIndex = 0;
             }
+          
             DisplayCurrentImage();
             UpdateImageCount();
         }
@@ -92,8 +107,91 @@ namespace UIDesignWPFApp
             string imagePath = imageFiles[currentImageIndex];
             BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
             imageControl.Source = bitmapImage;
+
+            
         }
 
 
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(canvas);
+
+            currentRectangle = new Rectangle
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 2,
+                Width = 0,
+                Height = 0
+            };
+
+            Canvas.SetLeft(currentRectangle, startPoint.X);
+            Canvas.SetTop(currentRectangle, startPoint.Y);
+
+            canvas.Children.Add(currentRectangle);
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && currentRectangle != null)
+            {
+                Point currentPoint = e.GetPosition(canvas);
+
+                double width = Math.Abs(currentPoint.X - startPoint.X);
+                double height = Math.Abs(currentPoint.Y - startPoint.Y);
+
+                Canvas.SetLeft(currentRectangle, Math.Min(startPoint.X, currentPoint.X));
+                Canvas.SetTop(currentRectangle, Math.Min(startPoint.Y, currentPoint.Y));
+
+                currentRectangle.Width = width;
+                currentRectangle.Height = height;
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (currentRectangle != null)
+            {
+                // Save the coordinates of the bounding box here
+                double x = Canvas.GetLeft(currentRectangle);
+                double y = Canvas.GetTop(currentRectangle);
+                double width = currentRectangle.Width;
+                double height = currentRectangle.Height;
+                Rect boundingBox = new Rect(x, y, width, height);
+                boundingBoxes.Add(boundingBox);
+
+                // Save the coordinates to a file or perform any other desired operation
+
+                currentRectangle = null;
+            }
+        }
+
+        
+
+        private void ClearCanvas()
+        {
+            canvas.Children.Clear();
+            boundingBoxes.Clear();
+        }
+
+        private void SaveBoundingBoxes()
+        {
+            string imagePath = imageFiles[currentImageIndex];
+            string fileName = System.IO.Path.GetFileNameWithoutExtension(imagePath) + ".txt";
+            string filePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(imagePath), fileName);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (Rect boundingBox in boundingBoxes)
+                {
+                    // Convert the bounding box coordinates to YOLO format
+                    double x = boundingBox.X / imageControl.ActualWidth;
+                    double y = boundingBox.Y / imageControl.ActualHeight;
+                    double width = boundingBox.Width / imageControl.ActualWidth;
+                    double height = boundingBox.Height / imageControl.ActualHeight;
+
+                    writer.WriteLine($"{x} {y} {width} {height}");
+                }
+            }
+        }
     }
 }
